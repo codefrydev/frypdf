3bd9791f7cf0fb675b78199ca4b054982eb41e18
PrashantUnity <priyadarshisadiku@gmail.com>
2026-09-06 23:52:30 +0530

    Just Page

--- src/PdfEditorApp.Core/Models/OutlineItem.cs
+++
@@ -1,8 +0,0 @@
-namespace PdfEditorApp.Core.Models;
-
-public class OutlineItem
-{
-    public string Title { get; set; } = "";
-    public int PageIndex { get; set; }
-    public string Kind { get; set; } = "Heading";
-}
--- src/PdfEditorApp/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace PdfEditorApp.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}

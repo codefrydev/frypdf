1f2fc6c7563bdc5a8309d0496b829bee5ee8e50e
PrashantUnity <priyadarshisadiku@gmail.com>
2026-09-11 08:38:04 +0530

    feat: add LoadingAnimationControl for improved user feedback during long-running operations

---
+++ src/PdfEditorApp/Views/Controls/LoadingAnimationControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PdfEditorApp.Views.Controls;
+
+public partial class LoadingAnimationControl : UserControl
+{
+    public LoadingAnimationControl()
+    {
+        InitializeComponent();
+    }
+}
